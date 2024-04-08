7e2102747d98222ce34ef804556f2f65a78b5bac
一颗小土豆 <72010312+potatoone@users.noreply.github.com>
2024-04-08 18:08:33 +0800

    Update env.ts

--- plugins/oidc/server/env.ts
+++ plugins/oidc/server/env.ts
@@ -71,6 +71,15 @@ class OIDCPluginEnvironment extends Environment {
     environment.OIDC_USERNAME_CLAIM ?? "preferred_username";
 
   /**
+   * The OIDC profile field to use for groups claim. If set, this should
+   * contain an array of group names (as strings). The groups will be created
+   * if missing and the user's group memberships will be updated accordingly.
+   */
+  public OIDC_GROUPS_CLAIM = this.toOptionalString(
+    environment.env.OIDC_GROUPS_CLAIM
+  );
+
+  /**
    * A space separated list of OIDC scopes to request. Defaults to "openid
    * profile email".
    */
